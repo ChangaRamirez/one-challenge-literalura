f4e6cd7ce9946ad07062d8f0654352290757e841
Changa Ramirez <laloraco@hotmail.com>
2025-08-07 09:42:12 -0600

    Convierte datos JSON a clases Java

---
+++ src/main/java/com/alura/challenge_literalura/principal/Principal.java
@@ -0,0 +1,17 @@
+package com.alura.challenge_literalura.principal;
+
+import com.alura.challenge_literalura.model.Datos;
+import com.alura.challenge_literalura.service.ConsumoAPI;
+import com.alura.challenge_literalura.service.ConvierteDatos;
+
+public class Principal {
+    private static final String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+    public void muestraElMenu() {
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+    }
+}
